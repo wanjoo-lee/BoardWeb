1e6e5f5ae8430727fe68c2dd9f4c50779239c781
wanjoo <wjlee@yi.ac.kr>
2018-05-22 09:38:41 +0900

    1st

---
+++ BoardWeb/src/main/java/polymorphism/LgTV.java
@@ -0,0 +1,16 @@
+package polymorphism;
+
+public class LgTV {
+	public void turnOn() {
+		System.out.println("LgTV--전원켠다.");
+	}
+	public void turnOff() {
+		System.out.println("LgTV--전원끈다.");
+	}
+	public void soundUp() {
+		System.out.println("LgTV--소리올린다.");
+	}
+	public void soundDown() {
+		System.out.println("LgTV--소리내린다.");
+	}
+}
---
+++ BoardWeb/src/main/java/polymorphism/SamsungTV.java
@@ -0,0 +1,16 @@
+package polymorphism;
+
+public class SamsungTV {
+	public void powerOn() {
+		System.out.println("SamsungTV--전원켠다.");
+	}
+	public void powerOff() {
+		System.out.println("SamsungTV--전원끈다.");
+	}
+	public void volumeUp() {
+		System.out.println("SamsungTV--소리올린다.");
+	}
+	public void volumeDown() {
+		System.out.println("SamsungTV--소리내린다.");
+	}
+}
